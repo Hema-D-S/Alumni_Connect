7a4cd80e2ed301a269d95dc98066973d53192341
Hema-D-S <sphemads@gmail.com>
2025-08-22 01:10:01 +0530

    Added bg

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    // Make sure the wrapper itself takes full width & height
+    <div className="bg-image">
+      <h1 style = {{ color:"white"}}>Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
