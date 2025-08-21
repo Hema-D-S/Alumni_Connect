9fae1cbeb58ecc25b7f31605fdefd37e5c405cbc
Hema-D-S <sphemads@gmail.com>
2025-08-22 02:38:50 +0530

    Landing Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,48 @@
-import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    // Make sure the wrapper itself takes full width & height
-    <div className="bg-image">
-      <h1 style = {{ color:"white"}}>Welcome</h1>
+    <div className="app">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo">Alumni Connect</div>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Alumni</a></li>
+          <li><a href="#">Research</a></li>
+          <li><a href="#">Campus Life</a></li>
+          <li><a href="#">Contact</a></li>
+          <li>
+              <a href="#" className="signin-btn">
+                {/* User icon SVG */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="white"
+         
+                  width="24"
+                  height="24"
+                >
+                  <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z"/>
+                </svg>
+              </a>
+            </li>
+
+        </ul>
+      </nav>
+
+      {/* Background */}
+      <div className="bg-image">
+        {/* Expanding border frame */}
+        <div className="border-frame"></div>
+
+        {/* Overlay text */}
+        <div className="overlay-text">
+          <h1>University of Visvesvaraya College of Engineering</h1>
+          <p>First State Autonomous Institute in Karnataka</p>
+          <button className="get-started-btn">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
