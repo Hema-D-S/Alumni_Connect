2b5c154ec1432b9b56dc5ae96c68bfec0516e781
Hema-D-S <sphemads@gmail.com>
2025-08-29 23:13:48 +0530

    deployment issue resolved

--- backend/server.js
+++ backend/server.js
@@ -88,7 +88,9 @@ app.set("io", io);
 
 // ---------------- Middleware ----------------
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://alumni-connect-oe7z.vercel.app", credentials: true })
+);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // ---------------- Routes ----------------
@@ -109,4 +111,4 @@ app.use("/api/messages", require("./routes/chatRoutes"));
 app.get("/", (req, res) => res.send("API running..."));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+module.exports = server;
