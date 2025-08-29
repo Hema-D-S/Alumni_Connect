3f0ca47ba71173322987731c211cd726008436d1
Hema-D-S <sphemads@gmail.com>
2025-08-29 22:59:15 +0530

    Added url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 // ---------------- Socket.IO Setup ----------------
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://alumni-connect-oe7z.vercel.app/",
     methods: ["GET", "POST"],
     credentials: true,
   },
